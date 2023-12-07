c41f9bcfa5b32ad8865925b4ed0f86249d025318
Jacob Gold <jacob@graphite.dev>
2023-12-06 22:17:59 -0800

    experimental ternaries

--- day7.ts
+++ day7.ts
@@ -23,19 +23,19 @@ const total = lines
     }
     const sorted = [...occurrences.values()].sort((a, b) => b - a);
     const rank =
-      sorted[0] === 1
-        ? 0 // high card
-        : sorted[0] === 2
-          ? sorted[1] === 2
-            ? 2 // two pair
-            : 1 // pair
-          : sorted[0] === 3
-            ? sorted[1] === 2
-              ? 4 // full house
-              : 3 // three of a kind
-            : sorted[0] === 4
-              ? 5 // four of a kind
-              : 6; // five of a kind
+      sorted[0] === 1 ?
+        0 // high card
+      : sorted[0] === 2 ?
+        sorted[1] === 2 ?
+          2 // two pair
+        : 1 // pair
+      : sorted[0] === 3 ?
+        sorted[1] === 2 ?
+          4 // full house
+        : 3 // three of a kind
+      : sorted[0] === 4 ?
+        5 // four of a kind
+      : 6; // five of a kind
     return { rank, cards, bet: +bet };
   })
   .sort((a, b) =>
@@ -91,64 +91,52 @@ const total2 = lines
     }
     const sorted = [...occurrences.values()].sort((a, b) => b - a);
     const pj =
-      sorted[0] === 1
-        ? 0 // high card
-        : sorted[0] === 2
-          ? sorted[1] === 2
-            ? 2 // two pair
-            : 1 // pair
-          : sorted[0] === 3
-            ? sorted[1] === 2
-              ? 4 // full house
-              : 3 // three of a kind
-            : sorted[0] === 4
-              ? 5 // four of a kind
-              : 6; // five of a kind
+      sorted[0] === 1 ?
+        0 // high card
+      : sorted[0] === 2 ?
+        sorted[1] === 2 ?
+          2 // two pair
+        : 1 // pair
+      : sorted[0] === 3 ?
+        sorted[1] === 2 ?
+          4 // full house
+        : 3 // three of a kind
+      : sorted[0] === 4 ?
+        5 // four of a kind
+      : 6; // five of a kind
     return {
       rank:
-        numJokers === 0
-          ? pj
-          : numJokers === 1
-            ? pj === 0
-              ? 1
-              : pj === 1
-                ? 3
-                : pj === 2
-                  ? 4
-                  : pj === 3
-                    ? 5
-                    : pj === 5
-                      ? 6
-                      : pj
-            : numJokers === 2
-              ? pj === 0
-                ? 3
-                : pj === 1
-                  ? 5
-                  : pj === 3
-                    ? 6
-                    : pj
-              : numJokers === 3
-                ? pj === 0
-                  ? 5
-                  : pj === 1
-                    ? 6
-                    : pj
-                : numJokers === 4
-                  ? pj === 0
-                    ? 6
-                    : pj
-                  : numJokers === 5
-                    ? 6
-                    : 0,
+        numJokers === 0 ? pj
+        : numJokers === 1 ?
+          pj === 0 ? 1
+          : pj === 1 ? 3
+          : pj === 2 ? 4
+          : pj === 3 ? 5
+          : pj === 5 ? 6
+          : pj
+        : numJokers === 2 ?
+          pj === 0 ? 3
+          : pj === 1 ? 5
+          : pj === 3 ? 6
+          : pj
+        : numJokers === 3 ?
+          pj === 0 ? 5
+          : pj === 1 ? 6
+          : pj
+        : numJokers === 4 ?
+          pj === 0 ?
+            6
+          : pj
+        : numJokers === 5 ? 6
+        : 0,
       cards,
       bet: +bet,
     };
   })
   .sort((a, b) =>
-    b.rank === a.rank
-      ? compareHandsByCard(a.cards, b.cards, rankMap2)
-      : a.rank! - b.rank!
+    b.rank === a.rank ?
+      compareHandsByCard(a.cards, b.cards, rankMap2)
+    : a.rank! - b.rank!
   )
   .reduce((acc, { bet }, i) => {
     return acc + (1 + i) * +bet;
