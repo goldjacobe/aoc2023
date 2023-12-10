25e56dd2c4aa39191ac0049338644fed577000e7
Jacob Gold <jacob@graphite.dev>
2023-12-09 20:57:04 -0800

    logs

--- day9/day9.ts
+++ day9/day9.ts
@@ -5,8 +5,6 @@ const input = fs.readFileSync(infile, 'utf8');
 
 const lines = input.trim().split('\n');
 
-console.log(lines);
-
 const values = lines.map((line) => {
   const numbers = line.split(/\s+/).map((s) => parseInt(s));
   const history = [];
@@ -39,13 +37,9 @@ const values2 = lines.map((line) => {
     history.push(current);
     current = next;
   }
-  console.log(history);
   return history.toReversed().reduce((acc, curr) => {
-    console.log(acc);
     return curr[0] - acc;
   }, 0);
 });
-
-console.log(values2);
 
 console.log(values2.reduce((acc, curr) => acc + curr, 0));
